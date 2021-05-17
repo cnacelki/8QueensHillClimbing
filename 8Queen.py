a87896c418e3188dddde0e1b61ac3fed179293f1
cnacelki <ccan.celik.1904@gmail.com>
2021-05-17 11:56:31 +0300

    d

--- 8Queen.py
+++ 8Queen.py
@@ -1,8 +1,16 @@
 from Board import Board
+restarts=[]
+moves=[]
+times=[]
 for i in range(25):
     b = Board(8)
     b.newBoard()
     move,restart,time = b.hillClimbing()
+    moves.append(move)
+    restarts.append(restart)
+    times.append(time)
     print("Restart :" ,restart)
     print("Move:" ,move)
     print("Time:",time)
+for i in range(25):
+    print("Board:{} Restart Count:{} Move Count:{} Time:{}".format(i,restarts[i],moves[i],times[i]))
